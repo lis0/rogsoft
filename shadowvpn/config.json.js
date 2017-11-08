f0d8dacfd7cad95e488e7e2a221961ccea79fb98
sadoneli <sadoneli@gmail.com>
2017-11-08 18:18:02 +0800

    remove some

--- shadowvpn/config.json.js
+++
@@ -1,8 +0,0 @@
-{
-"version":"2.9",
-"md5":"2f156357fb324562eda66410a6b2ad64",
-"home_url":"Module_shadowvpn.asp",
-"title":"Shadowvpn",
-"description":"SVPN让游戏更畅快",
-"build_date":"2016-11-25_21:41:36"
-}
--- speedtest/speedtest/scripts/speedtest_uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -f /koolshare/scripts/speedtest.sh
-rm -f /koolshare/bin/speedtest
-rm -f /koolshare/webs/Module_speedtest.asp
