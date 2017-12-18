83e777049869c6f410209e99a8d1c58006c83121
sadoneli <sadoneli@gmail.com>
2017-12-18 22:59:00 +0800

    update message#3

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,5 +1,5 @@
 {
-"title":"<em><% nvram_get('model'); %>软件中心&nbsp;-&nbsp;by&nbsp;koolshare</em>",
+"title":"<em>软件中心&nbsp;-&nbsp;by&nbsp;koolshare</em>",
 "content1":"有任何问题，欢迎到我们的论坛：<a href='http://koolshare.cn' target='_blank'><u>http://koolshare.cn</u></a>或者软件中心源地址：<a href='https://github.com/koolshare/rogsoft' target='_blank'><u>https://github.com/koolshare/rogsoft</u></a>反馈。",
 "content2":"1.2.0更新日志：修复插件的开机启动。"
 }
