1592e5b539d6d0f5d89f8c0904c3077429a32951
sadoneli <sadoneli@gmail.com>
2018-08-13 10:54:09 +0800

    update message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,9 +1,11 @@
 {
 "title":"<em>软件中心&nbsp;-&nbsp;by&nbsp;koolshare</em>",
 "content1":"1.3.2更新日志：新增86U对Enable JFFS custom scripts and configs选项是否开启的检测。建议升级！",
-"content2":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
+"content2":"<b>koolproxy 8月13日断网解决办法：<b><a href='http://koolshare.cn/thread-145095-1-1.html' target='_blank'> <em>http://koolshare.cn/thread-145095-1-1.html</em> </a>",
+"content3":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
 <a href='https://github.com/koolshare/rogsoft' target='_blank'><em>1.软件中心GitHub项目</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/xbchat' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='http://shang.qq.com/wpa/qunwpa?idkey=f475468129ba8019245425559b5df5bdad7d7201ac7780417dd0218bbb4e1322' target='_blank'><em>3.加入QQ群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='http://koolshare.cn/forum-98-1.html' target='_blank'><em>4.Koolshare论坛插件版块</em></a>"
 }
+
