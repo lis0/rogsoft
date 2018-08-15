40923a76bca0108f00d38d31a2f4c1c4650c5a18
sadoneli <sadoneli@gmail.com>
2018-08-15 11:21:08 +0800

    upate msg

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,7 @@
 {
 "title":"<em>软件中心&nbsp;-&nbsp;by&nbsp;koolshare</em>",
 "content1":"1.3.2更新日志：新增86U对Enable JFFS custom scripts and configs选项是否开启的检测。建议升级！",
-"content2":"<b>koolproxy 8月13日断网解决办法：<b><a href='http://koolshare.cn/thread-145095-1-1.html' target='_blank'> <em>http://koolshare.cn/thread-145095-1-1.html</em> </a>",
+"content2":"<b>koolproxy重要通告：<b><a href='http://koolshare.cn/thread-145260-1-1.html' target='_blank'> <em>http://koolshare.cn/thread-145260-1-1.html</em> </a>",
 "content3":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
 <a href='https://github.com/koolshare/rogsoft' target='_blank'><em>1.软件中心GitHub项目</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/xbchat' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
