e6a0c0225ddccbaf48acdb40fb77e68bccaa70a5
sadoneli <sadoneli@gmail.com>
2018-09-01 19:33:14 +0800

    update rog

--- rog/rog/scripts/rog_status.sh
+++ rog/rog/scripts/rog_status.sh
@@ -18,9 +18,9 @@ GT-AC5300)
 	interface_2g=`nvram get wl0_ifname`
 	interface_5g1=`nvram get wl1_ifname`
 	interface_5g2=`nvram get wl2_ifname`
-	interface_2g_temperature=`wl -i ${interface_2g} phy_tempsense | awk '{print $1}'`
-	interface_5g1_temperature=`wl -i ${interface_5g1} phy_tempsense | awk '{print $1}'`
-	interface_5g2_temperature=`wl -i ${interface_5g2} phy_tempsense | awk '{print $1}'`
+	interface_2g_temperature=`wl -i ${interface_2g} phy_tempsense | awk '{print $1}'` 2>/dev/null
+	interface_5g1_temperature=`wl -i ${interface_5g1} phy_tempsense | awk '{print $1}'` 2>/dev/null
+	interface_5g2_temperature=`wl -i ${interface_5g2} phy_tempsense | awk '{print $1}'` 2>/dev/null
 	[ -n "${interface_2g_temperature}" ] && interface_2g_temperature_c=`expr ${interface_2g_temperature} / 2 + 20`°C || interface_2g_temperature_c="offline"
 	[ -n "${interface_5g1_temperature}" ] && interface_5g1_temperature_c=`expr ${interface_5g1_temperature} / 2 + 20`°C || interface_5g1_temperature_c="offline"
 	[ -n "${interface_5g2_temperature}" ] && interface_5g2_temperature_c=`expr ${interface_5g2_temperature} / 2 + 20`°C || interface_5g2_temperature_c="offline"
@@ -29,8 +29,8 @@ GT-AC5300)
 RT-AC86U)
 	interface_2g=`nvram get wl0_ifname`
 	interface_5g1=`nvram get wl1_ifname`
-	interface_2g_temperature=`wl -i ${interface_2g} phy_tempsense | awk '{print $1}'`
-	interface_5g1_temperature=`wl -i ${interface_5g1} phy_tempsense | awk '{print $1}'`
+	interface_2g_temperature=`wl -i ${interface_2g} phy_tempsense | awk '{print $1}'` 2>/dev/null
+	interface_5g1_temperature=`wl -i ${interface_5g1} phy_tempsense | awk '{print $1}'` 2>/dev/null
 	[ -n "${interface_2g_temperature}" ] && interface_2g_temperature_c=`expr ${interface_2g_temperature} / 2 + 20`°C || interface_2g_temperature_c="offline"
 	[ -n "${interface_5g1_temperature}" ] && interface_5g1_temperature_c=`expr ${interface_5g1_temperature} / 2 + 20`°C || interface_5g1_temperature_c="offline"
 	wl_temperature="2.4G：${interface_2g_temperature_c} &nbsp;&nbsp;|&nbsp;&nbsp; 5G-1：${interface_5g1_temperature_c}"
